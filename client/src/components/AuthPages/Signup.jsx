b0e9bd7f70eb6d5320fdf4b881d2d48b526b07fe
icodervivek <vivekpramanikbundu@gmail.com>
2025-10-13 15:09:38 +0530

    feat: added more inputs

--- client/src/components/AuthPages/Signup.jsx
+++ client/src/components/AuthPages/Signup.jsx
@@ -4,24 +4,41 @@ import Footer from "../Footer";
 import axios from "axios";
 import { Bounce, ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import cors from "cors"
+import cors from "cors";
+import { useEffect } from "react";
 
 const Signup = () => {
   const navigate = useNavigate();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [experience, setExperience] = useState("");
+  const [previousJob, setPreviousJob] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+
+
+  useEffect(() => {
+        document.title = "Sign Up - SwiftHire";
+      }, []);
+
 
   const handleSignup = async (e) => {
     e.preventDefault();
 
     try {
       const response = await axios.post(
-        "http://localhost:3000/signup"
-        ,
-        { name, email, password },
+        "http://localhost:3000/signup",
+        {
+          name,
+          email,
+          password,
+          experience,
+          previous_job_role: previousJob,
+          contact_number: contactNumber,
+        },
         { withCredentials: true }
       );
+
       toast.success(response.data.message || "Signup Successful", {
         position: "top-center",
         autoClose: 2000,
@@ -48,9 +65,7 @@ const Signup = () => {
         <div className="w-full max-w-7xl flex flex-col md:flex-row items-center justify-center gap-10">
           {/* Left Section: Form */}
           <div className="form-section w-full md:w-1/2 max-w-md p-8 rounded-lg shadow-md">
-            <h2 className="text-3xl font-bold mb-6 text-center">
-              Sign Up
-            </h2>
+            <h2 className="text-3xl font-bold mb-6 text-center">Sign Up</h2>
             <form className="flex flex-col gap-4" onSubmit={handleSignup}>
               <input
                 type="text"
@@ -76,6 +91,31 @@ const Signup = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
+              <input
+                type="number"
+                name="experience"
+                placeholder="Experience (months)"
+                className="p-3 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+                value={experience}
+                onChange={(e) => setExperience(e.target.value)}
+              />
+              <input
+                type="text"
+                name="previousJob"
+                placeholder="Previous Job Role"
+                className="p-3 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+                value={previousJob}
+                onChange={(e) => setPreviousJob(e.target.value)}
+              />
+              <input
+                type="text"
+                name="contactNumber"
+                placeholder="Contact Number"
+                className="p-3 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+                value={contactNumber}
+                onChange={(e) => setContactNumber(e.target.value)}
+              />
+
               <button
                 type="submit"
                 className="bg-green-500 hover:bg-green-600 cursor-pointer transition tracking-widest text-white py-3 rounded text-lg"
